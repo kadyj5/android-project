c0ee1212a3ce2f51770358c6bb8cadc14103f94e
kadyj5 <karolina.dyjak5@gmail.com>
2023-03-24 16:05:37 +0100

    Initial commit

--- app/src/main/java/com/example/android_project/MainActivity.kt
+++ app/src/main/java/com/example/android_project/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.android_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button:Button = findViewById(R.id.callbutton)
+        val editText:EditText = findViewById(R.id.editText_view)
+
+        button.setOnClickListener {
+            val phoneNumber = editText.text.toString()
+            Toast.makeText(this,phoneNumber,Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
